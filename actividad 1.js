f7a2e7c5f34d1a875e99d36cf49255a27444046f
Hector-El-Father <ramirez.rendonhector@gmail.com>
2022-07-05 13:27:07 -0500

    main

---
+++ actividad 1.js
@@ -0,0 +1,24 @@
+// problema 1:
+var numero=prompt("ingrese un numero")
+undefined
+var cantidad=2
+undefined
+var total=numero*cantidad
+console.log(total)
+VM1691:2 4
+
+// problema 2:
+
+// problema 3:
+const radio=5.3*5.3
+var pi=3.1416
+var total=radio*pi
+console.log(total)
+VM952:4 88.247544
+//problema 4:
+var externo=8
+var interno=6
+const dos=2
+var total=externo*interno/dos
+console.log(total)
+VM2344:5 24
