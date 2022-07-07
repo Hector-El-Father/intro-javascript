228727cef629d42a8ced36679d7175c0bf1db9b9
Hector-El-Father <ramirez.rendonhector@gmail.com>
2022-07-06 22:34:26 -0500

    main

---
+++ actividad 3.js
@@ -0,0 +1,30 @@
+// problema 1
+var nombre="Mario";
+var edad=17;
+
+if(edad>=18 && nombre=="carlos"){
+    console.log("puede entrar")
+} if(edad>=18 && nombre=="Mario"){
+    console.log("puede entrar")
+} else if(edad<=18 && nombre=="carlos"){
+    console.log("no puede entrar")
+} else if(edad<=18 && nombre=="Mario"){
+    console.log("no puede entrar")
+}
+// problema 2
+var jugador1="piedra";
+var jugador2="papel";
+
+if(jugador1=="piedra"){
+    console.log("gana jugador1")
+}
+
+// problema 5
+var numero=100;
+
+if(numero==1000){
+    console.log("ganaste un premio")
+} else if(numero<=1000){
+    console.log("Lo sentimos, sigue participando")
+}
+// problema 6
