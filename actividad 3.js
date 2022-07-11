13d643f0c0961b586590005a2ca8bfd8280b82ae
Hector-El-Father <ramirez.rendonhector@gmail.com>
2022-07-10 23:02:33 -0500

    main

--- actividad 3.js
+++ actividad 3.js
@@ -1,30 +1,66 @@
 // problema 1
-var nombre="Mario";
-var edad=17;
-
-if(edad>=18 && nombre=="carlos"){
-    console.log("puede entrar")
-} if(edad>=18 && nombre=="Mario"){
-    console.log("puede entrar")
-} else if(edad<=18 && nombre=="carlos"){
-    console.log("no puede entrar")
-} else if(edad<=18 && nombre=="Mario"){
-    console.log("no puede entrar")
+var edad=prompt('¿Que edad tienes?');
+if(edad>=18){
+    var nombre=prompt('¿Cual es tu nombre?');
+    if(nombre=="Mario" || nombre=="Carlos"){
+        alert('entra al VIP: '+nombre);
+    } else{
+        alert('Entra a la discoteca: '+nombre);
+    }
+}
+else{
+    alert('no puedes entrar a la discoteca: '+nombre);
 }
 // problema 2
-var jugador1="piedra";
-var jugador2="papel";
-
+var jugador1=prompt("escoge piedra, papel o tijera");
 if(jugador1=="piedra"){
-    console.log("gana jugador1")
+    var jugador2=prompt("escoge piedra, papel o tijera");
+    if(jugador2=="piedra"){
+        alert("empate")
+    } else(jugador2=="papel"){
+        alert("gana jugador")
+    }
+    
 }
 
 // problema 5
-var numero=100;
-
+var numero=prompt("escoge un numero")
 if(numero==1000){
-    console.log("ganaste un premio")
+    alert("ganaste un premio")
 } else if(numero<=1000){
-    console.log("Lo sentimos, sigue participando")
+    alert(numero+", Lo sentimos, sigue participando")
+} else if(numero>=1000){
+    alert(numero+", Lo sentimos, sigue participando")
 }
 // problema 6
+var num1=prompt("escoge un numero");
+var num2=prompt("escoge un numero");
+if(num1>num2){
+    alert("numero "+ num1 +" es mayor que numero "+num2);
+} else if(num1<num2){
+    alert("numero "+ num1 +" es menor que numero "+num2);
+}
+// problema 7
+var num1=prompt("escoge un numero");
+var num2=prompt("escoge un numero");
+var num3=prompt("escoge un numero");
+if(num1>num2 && num1>num3){
+    alert("numero "+num1+" es mayor que numero "+num2+" y " + num3);
+} if(num2>num1 && num2>num3){
+    alert("numero "+num2+" es mayor que numero "+num1+" y " + num3);
+} if(num3>num1 && num3>num1){
+    alert("numero "+num3+" es mayor que numero "+num1+" y " + num2);
+} if(num1==num2 && num1>num3){
+    alert("numero "+num1+" es igual a "+num2+" y son mayor que " + num3);
+} if(num2==num1 && num2>num3){
+    alert("numero "+num2+" es igual a "+num1+" y son mayor que " + num3);
+} else if(num1<num2 && num1<num3){
+    alert("numero "+num1+" es menor que numero "+num2+" y " + num3);  
+} else if(num2<num1 && num2<num3){
+    alert("numero "+num2+" es menor que numero "+num1+" y " + num3); 
+} else if(num3<num1 && num3<num1){
+    alert("numero "+num3+" es menor que numero "+num1+" y " + num2); 
+}
+
+
+
